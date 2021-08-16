808ceb2c426f5df598c86cae4d63e0fa271a42ab
Laura HL <40395141+lamusadecima@users.noreply.github.com>
2021-08-16 19:29:37 +0200

    Add files via upload

---
+++ taller4/taller4.R
@@ -0,0 +1,29 @@
+#################################################
+##                                             ##
+##        DEL CORPUS A LA INTERPRETACIÓN:      ##
+##        ESTILOMETRÍA CON R                   ##
+##                                             ##
+##       Taller 4: Intro a la Estilometría     ##
+##                                             ##
+#################################################
+
+
+# Primero establecemos el directorio de trabajo
+# Si tienes un Mac y has guardado la carpeta "novela" en el escritorio:
+setwd("Desktop/novela")
+# Si tienes Windows y has guardado la carpeta en C:
+setwd("C:/novela")
+
+# Si las líneas de arriba te dan error, otra opción para establecer el 
+#directorio de trabajo es hacer clic en Session > Set working directory
+
+# Si todavía no has instalado stylo, ejecuta esta línea:
+install.packages("stylo")
+
+# A continuación, llamamos al paquete stylo
+
+library(stylo)
+
+# Para funciones básicas de Estilometría
+
+stylo()
